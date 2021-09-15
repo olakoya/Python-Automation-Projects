edc85e9e781454ad9ecd8ab15799002928fa101e
olakoya <68306137+olakoya@users.noreply.github.com>
2021-09-15 01:46:04 +0100

    Automate Croawco Project

---
+++ TestCase1.py
@@ -0,0 +1,45 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common import keys
+
+URL = "https://www.crawco.co.uk/"
+
+driver = webdriver.Chrome(r'C:\chromedriver.exe')
+driver.get(URL)
+
+fb_btn = driver.find_element_by_class_name("fa-facebook-f")
+fb_btn.click()
+
+driver.implicitly_wait(30)
+
+# confirm_not_now = driver.find_element_by_id("expanding_cta_close_button")
+# confirm_not_now.click()
+
+# driver.implicitly_wait(10)
+
+# fb_login = driver.find_element_by_class_name("_4jy1")
+# fb_login.click()
+
+# driver.implicitly_wait(10)
+
+def login(username):
+    user = driver.find_element_by_css_selector("#email")
+    user.clear()
+    user.send_keys(username)
+
+def passwd(password):
+    passw = driver.find_element_by_css_selector("#pass")
+    passw.clear()
+    passw.send_keys(password)
+
+driver.implicitly_wait(10)
+login("boldlord@gmail.com")
+passwd("Pejulord123@")
+
+driver.implicitly_wait(10)
+
+submit = driver.find_element_by_name("login")
+submit.click()
+time.sleep(5)
+driver.close()
